ca21be5e3949d71c3af4c5c7da48d859006b158e
Alex Terenda <alexterenda@gmail.com>
2021-04-13 10:58:50 +0100

    Home on mount animation

--- framer-motion-pizza/src/components/Home.js
+++ framer-motion-pizza/src/components/Home.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      className='home container'
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: '#ff2994' }}>
         Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
+      </motion.h2>
+      <Link to='/base'>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
