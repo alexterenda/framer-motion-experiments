08ae863a2e07843cb32ee098d28d43f823ffc5e8
Alex Terenda <alexterenda@gmail.com>
2021-05-03 07:41:41 +0100

    Added modal, added exit animation

--- framer-motion-pizza/src/components/Modal.js
+++ framer-motion-pizza/src/components/Modal.js
@@ -7,6 +7,20 @@ const backdropVariants = {
   hidden: { opacity: 0 },
 };
 
+const modalVariants = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0,
+  },
+  visible: {
+    y: '200px',
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
 const Modal = ({ showModal, setShowModal }) => {
   return (
     <AnimatePresence>
@@ -16,7 +30,15 @@ const Modal = ({ showModal, setShowModal }) => {
           variants={backdropVariants}
           animate='visible'
           initial='hidden'
-        ></motion.div>
+          exit='hidden'
+        >
+          <motion.div className='modal' variants={modalVariants}>
+            <p>Want to make another pizza?</p>
+            <Link to='/'>
+              <button onClick={() => setShowModal(false)}>Start again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
